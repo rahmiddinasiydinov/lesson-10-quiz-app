37f12c74b3cdc77a94e8ab4616c8e62cd926810a
rahmiddin <rahmiddinasiydinov@gmail.com>
2025-07-04 16:08:56 +0500

    finish section 12

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,27 +1,35 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import QUESTIONS from '../questions';
+import Question from "./Question";
+import Summary from "./Summary";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setUserAnswers(prevAnswers => {
             return [...prevAnswers, selectedAnswer];
-        })
+        });
+    }, [activeQuestionIndex])
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
+
+    if (quizIsComplete) {
+        return <Summary userAnswers={userAnswers}/>
     }
 
+
+
+
     return <div id="quiz">
-        <div id="question">
-            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-            <ul id="answers">
-                {QUESTIONS[activeQuestionIndex].answers.map(answer => (
-                    <li key={answer} className="answer">
-                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <Question 
+        key={activeQuestionIndex}
+        index={activeQuestionIndex }
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+        />
     </div>
 }
